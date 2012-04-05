dbfe346628f68eec7f7493aa588b606b4ddedc43
Aneesh Dogra <lionaneesh@gmail.com>
2012-04-05 13:20:05 +0530

    Remove useless comments and Do not print pages on every sync

--- crawler_db_handling.py
+++ crawler_db_handling.py
@@ -14,16 +14,6 @@ def db_setup_everything(c, con):
     except lite.OperationalError:
         error("Creating cache", 2)        
         c.execute("CREATE TABLE cache(url text, content text)")
-#    try:
-#        c.execute('SELECT * from crawled')
-#    except lite.OperationalError:
-#        c.execute("CREATE TABLE crawled(url text)")
-#    try:
-#        c.execute('SELECT * from to_crawl')
-#    except lite.OperationalError:
-#        c.execute("CREATE TABLE to_crawl(url text)")
-#        c.execute("INSERT INTO to_crawl values(\'%s\')" % ('http://google.com',))
-
     con.commit()
 
 def db_get_data(pages, c):
@@ -36,20 +26,3 @@ def db_get_data(pages, c):
         except TypeError:
             break
         pages[data[0]] = data[1]
-
-    print pages
-
-#    c.execute('SELECT * from crawled')
-#    data = c.fetchone()
-    
-    #build up the lists
-#    while data != None:    
-#        crawled.append(data)
-#        crawled_pages.append(data[1])
-#    
-#    c.execute('SELECT * from to_crawl')
-#    data = c.fetchone()    
-#
-#    while data != None:    
-#        to_crawl.append("%s" % data)
-#        data = c.fetchone()
